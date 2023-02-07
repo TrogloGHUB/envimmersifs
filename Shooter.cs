2f444aefb9b514c2b5e58cba499b153f9a842688
François Mercier <94062009+TrogloGHUB@users.noreply.github.com>
2023-02-07 18:11:26 -0500

    Add files via upload
    Pour exemple petit jeu de casse-briques

---
+++ Shooter.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+    Permet de tirer des projectiles à partir de l'objet placé dans shotPos.
+    Utiliser des valeurs entre 100 et 200 pour shotForce (tout dépendant de la taille des objets).
+
+    Utiliser une masse entre 0.5 et 1.5 pour le projectile qui sera Instantié.
+*/
+
+public class Shooter : MonoBehaviour
+{
+    public Rigidbody projectile;
+    public Transform shotPos;
+    public float shotForce;
+    public float moveSpeed = 10f;
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float v = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        transform.Translate(new Vector3(h, v, 0f));
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            Rigidbody shot = Instantiate(projectile, shotPos.position, shotPos.rotation) as Rigidbody;
+            shot.AddForce(shotPos.forward * shotForce);
+
+            Destroy(shot.gameObject, 3f);
+        }
+    }
+}
